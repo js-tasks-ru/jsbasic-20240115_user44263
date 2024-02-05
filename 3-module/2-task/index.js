6c2b5f6881cf3f3abd724766f035cacae8f3ebb4
fedor <i@fmelihov.ru>
2024-02-05 20:03:28 +0300

    3 module 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(function(item) {
+    return item >= a && item <= b;
+  });
 }
+
+let arr = [5, 3, 8, 1, 2, 9, 4];
+let filtered = filterRange(arr, 3, 5);
+console.log(filtered); // [5, 3, 4]
+console.log(arr);
